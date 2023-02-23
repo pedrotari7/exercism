a2770e16bea588820bd0d0e13cdd5c8ba3138382
Pedro Alvito <pedrotari7@gmail.com>
2023-02-23 13:14:06 +0100

    typescript: roman-numberals

---
+++ typescript/roman-numerals/roman-numerals.ts
@@ -0,0 +1,26 @@
+export const toRoman = (n: number): string => {
+  const roman: Record<string, number> = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  }
+  let str = ''
+
+  for (const i of Object.keys(roman)) {
+    const q = Math.floor(n / roman[i])
+    n -= q * roman[i]
+    str += i.repeat(q)
+  }
+
+  return str
+}
